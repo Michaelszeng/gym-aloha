7fd05ea376f5b120f1a0ddf3d33edf2142b0be8e
Michael Zeng <michaelszeng@gmail.com>
2025-11-16 03:01:02 -0500

    update supercloud training config

--- train_ppo_supercloud.sh
+++ train_ppo_supercloud.sh
@@ -49,9 +49,9 @@ fi
 python ppo/train_ppo.py \
     ${RESUME_FROM:+--resume-from $RESUME_FROM} \
     --total-timesteps 100000000 \
-    --n-envs 96 \
+    --n-envs 128 \
     --n-steps 8192 \
-    --batch-size 98304 \
+    --batch-size 131072 \
     --n-epochs 15 \
     --learning-rate 3e-4 \
     --gamma 0.99 \
